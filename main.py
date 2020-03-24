22f78783c971edc53c5cd2be280baf159e17ebc1
matix0508 <matix0508@gmail.com>
2020-03-24 01:53:06 +0100

    working frame

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import tkinter as tk
 
 LARGE_FONT = ("Verdana", 12)
 
+
+
 class SeaofBTCapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
-         container.pack(side="top", fill="both", expand=True)
+        container.pack(side="top", fill="both", expand=True)
 
-         container.grid_rowconfigure(0, weight=1)
-         container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
@@ -32,3 +34,8 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="StartPage", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+
+app = SeaofBTCapp()
+app.mainloop()
