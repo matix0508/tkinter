0c32db0b2cf236fae7d70554b767075514796d3d
matix0508 <matix0508@gmail.com>
2020-03-27 11:56:21 +0100

    matplotlib added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import matplotlib
+
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import tkinter as tk
 from tkinter import ttk
+
+matplotlib.use("TkAgg")
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -18,13 +25,12 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
-
+        for F in (StartPage, PageOne, PageTwo, PageThree):
             frame = F(container, self)
 
             self.frames[F] = frame
 
-            frame.grid(row=0, column = 0, sticky="nsew")
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -41,13 +47,16 @@ class StartPage(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Visit Page1",
-                            command=lambda: controller.show_frame(PageOne))
+                             command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
         button2 = ttk.Button(self, text="Visit Page2",
-                            command=lambda: controller.show_frame(PageTwo))
+                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
+        button3 = ttk.Button(self, text="Visit Graph",
+                             command=lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 
 class PageOne(tk.Frame):
@@ -58,11 +67,11 @@ class PageOne(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Visit Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
+                             command=lambda: controller.show_frame(PageTwo))
         button1.pack()
 
         button2 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                             command=lambda: controller.show_frame(StartPage))
         button2.pack()
 
 
@@ -73,13 +82,35 @@ class PageTwo(tk.Frame):
         label = ttk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
         button2 = ttk.Button(self, text="Visit Page One",
-                            command=lambda: controller.show_frame(PageOne))
+                             command=lambda: controller.show_frame(PageOne))
         button2.pack()
 
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="GraphPage", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = ttk.Button(self, text="Back to Home",
+                             command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
 
