b25282b06279f1fa9211da7fe5a750774d918393
matix0508 <matix0508@gmail.com>
2020-03-28 00:00:29 +0100

    new branch CoronaVirus

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import matplotlib
-
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-import tkinter as tk
-from tkinter import ttk
 import matplotlib.animation as animation
 from matplotlib import style
 
-matplotlib.use("TkAgg")
+
+import tkinter as tk
+from tkinter import ttk
+
+import pandas as pd
 
 LARGE_FONT = ("Verdana", 12)
 style.use("ggplot")
@@ -18,66 +19,42 @@ a = f.add_subplot(111)
 
 
 def animate(i):
-
-    with open("sampleData.txt", "r") as data:
-        dataList = data.read().split("\n")
-        xList = []
-        yList = []
-        for eachLine in dataList:
-            if len(eachLine) > 1:
-                x, y = eachLine.split(",")
-                xList.append(int(x))
-                yList.append(int(y))
+    data = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/total_cases.csv")
     a.clear()
-    a.plot(xList, yList)
+    a.plot(data[loc][data[loc] != 0])
 
 
-class SeaofBTCapp(tk.Tk):
+def change_name(new):
+    name = new
+
+
+class CoronaVirus(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        tk.Tk.wm_title(self, "Sea of BTC Client")
+        tk.Tk.wm_title(self, "CoronaVirus Cases")
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo, PageThree):
+        for F in (PageOne, GraphPage):
             frame = F(container, self)
 
             self.frames[F] = frame
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(PageOne)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="StartPage", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Visit Page1",
-                             command=lambda: controller.show_frame(PageOne))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Visit Page2",
-                             command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-        button3 = ttk.Button(self, text="Visit Graph",
-                             command=lambda: controller.show_frame(PageThree))
-        button3.pack()
 
 
 class PageOne(tk.Frame):
@@ -87,39 +64,24 @@ class PageOne(tk.Frame):
         label = ttk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Visit Page Two",
-                             command=lambda: controller.show_frame(PageTwo))
+        button1 = ttk.Button(self, text="See the graph",
+                             command=lambda: controller.show_frame(GraphPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button2.pack()
 
-
-class PageTwo(tk.Frame):
+class GraphPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page Two", font=LARGE_FONT)
+        label = ttk.Label(self, text="Graph Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Visit Page One",
                              command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-
-class PageThree(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="GraphPage", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
+        button2 = ttk.Button(self, text="Poland",
+                             command=lambda: change_name("Poland"))
+        button2.pack()
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
@@ -130,6 +92,6 @@ class PageThree(tk.Frame):
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
-app = SeaofBTCapp()
+app = CoronaVirus()
 ani = animation.FuncAnimation(f, animate, interval=1000)
 app.mainloop()
